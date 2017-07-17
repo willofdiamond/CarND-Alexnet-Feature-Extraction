7367da52f93f8259879930d1889840319725f500
willofdiamond <hamcb@mst.edu>
2017-07-17 01:10:26 -0400

    half code is done

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,7 +19,9 @@ fc7 = AlexNet(resized, feature_extract=True)
 # HINT: Look at the final layer definition in alexnet.py to get an idea of what this
 # should look like.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+fc8W = tf.Variable(tf.random_normal(shape,stddev=1e-3))
+fc8b = tf.Variable(tf.zeros(nb_classes))
+probs = tf.nn.softmax(tf.matmul(fc7,fc8W)+fc8b)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -4,17 +4,26 @@ from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
 
 # TODO: Load traffic signs data.
-
+file_location = "D:/Udacity_selfdriving/CarND-Alexnet-Feature-Extraction/"
+file_name = "train.p"
+input_data = pickle.load(open(file_location+file_name,"rb"))
+print(input_data["features"].shape)
+X_data = input_data["features"]
+y_labels = input_data["labels"]
 # TODO: Split data into training and validation sets.
+X_train,y_train,X_test,y_test = train_test_split(input_data,input_labels,test_size = 0.3,random_state = 10)
 
 # TODO: Define placeholders and resize operation.
+features = tf.placeholder(tf.float32,(None,32,32,3))
+resized = tf.image.resize(features,(227,227))
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
+fc7 = AlexNet(resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
+
 
 # TODO: Add the final layer for traffic sign classification.
 
